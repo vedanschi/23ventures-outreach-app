0c36b20f5fb2ff4c41e086300e9115ff468413b7
Vedanshi <142626349+vedanschi@users.noreply.github.com>
2025-05-21 02:35:12 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { supabase } from '../../supabaseClient';
+import { supabase } from '../supabaseClient'
 
 export default function Layout({ children }) {
   const location = useLocation();
